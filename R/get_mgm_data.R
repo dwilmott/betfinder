666a24e640e1819b343bcef6af1f36badc6b5b6a
johnlovesdata <jschroed55@gmail.com>
2022-11-14 11:05:36 -0600

    update mgm back to using game json from github

--- R/get_mgm_data.R
+++ R/get_mgm_data.R
@@ -2,20 +2,19 @@ get_mgm_data <- function(sport, save_path = NULL, sleep_time = 0) {
 
   # set the main_URI by sport
   if (sport == 'nba') {
-    today_no_dashes <- gsub('-', '', as.character(Sys.Date()))
-    fixture_path <- paste0('https://raw.githubusercontent.com/jimtheflash/gambling_stuff/main/data/01_raw/mgm_nba_fixture_ids/',
-                           today_no_dashes,
-                           '.csv')
-    fixtures <- unlist(read.csv(fixture_path, header = FALSE))
+    git_trees <- jsonlite::fromJSON('https://api.github.com/repos/jimtheflash/gambling_stuff/git/trees/main?recursive=1')
+    tree <- git_trees$tree
+    blobs <- tree[tree$type == 'blob', ]
+    path_df <- blobs[grepl('data/01_raw/mgm_jsons', blobs$path), ]
+    paths <- path_df$path
 
   } else {
     stop('sport not supported')
   }
   # loop through those files and output some lists of event lists
   event_list <- list()
-  for (f in fixtures) {
-    ## this is a clunky fix and requires that we're getting fixture id's consistently, and also that mgm isn't changing up their api's on us
-    game_event <- jsonlite::fromJSON(paste0('https://cds-api.itsfogo.com/bettingoffer/fixture-view?x-bwin-accessid=ZTg4YWEwMTgtZTlhYy00MWRkLWIzYWYtZjMzODI5ZDE0Mjc5&lang=en-us&country=US&userCountry=US&offerMapping=All&scoreboardMode=Full&state=Latest&fixtureIds=', f), flatten = TRUE)
+  for (p in paths) {
+    game_event <- jsonlite::fromJSON(paste0('https://raw.githubusercontent.com/jimtheflash/gambling_stuff/main/', p), flatten = TRUE)
     event_list[[length(event_list) + 1]] <- game_event
 
     if (!is.null(save_path)) {
